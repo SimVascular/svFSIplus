b379d1a4ae77895f693a7812ae22aa78d4c56b4e
Dave Parker <dvdpax@gmail.com>
2023-05-18 19:09:26 -0700

    Solid viscosity bug 35 (#55)
    * Convert new ustruct code.
    
    * Add solid viscosity parameter, fix indexing problem.
    
    ---------
    
    Co-authored-by: Matteo Salvador <44835004+MatteoSalvador@users.noreply.github.com>

--- Code/Source/svFSI/ustruct.h
+++ Code/Source/svFSI/ustruct.h
@@ -21,7 +21,8 @@ int get_col_ptr(ComMod& com_mod, const int rowN, const int colN);
 void ustruct_2d_c(ComMod& com_mod, CepMod& cep_mod, const bool vmsFlag, const int eNoNw, const int eNoNq,
     const double w, const double Je, const Vector<double>& Nw,  const Vector<double>& Nq,
     const Array<double>& Nwx, const Array<double>& Nqx, const Array<double>& al, const Array<double>& yl,
-    const Array<double>& dl, const Array<double>& bfl, Array<double>& lR, Array3<double>& lK, Array3<double>& lKd);
+    const Array<double>& dl, const Array<double>& bfl, const Array<double>& Kxi, Array<double>& lR, Array3<double>& lK, 
+    Array3<double>& lKd);
 
 void ustruct_2d_m(ComMod& com_mod, CepMod& cep_mod, const bool vmsFlag, const int eNoNw, const int eNoNq,
     const int nFn, const double w, const double Je, const Vector<double>& Nw,  const Vector<double>& Nq,
@@ -32,7 +33,8 @@ void ustruct_2d_m(ComMod& com_mod, CepMod& cep_mod, const bool vmsFlag, const in
 void ustruct_3d_c(ComMod& com_mod, CepMod& cep_mod, const bool vmsFlag, const int eNoNw, const int eNoNq,
     const double w, const double Je, const Vector<double>& Nw,  const Vector<double>& Nq,
     const Array<double>& Nwx, const Array<double>& Nqx, const Array<double>& al, const Array<double>& yl, 
-    const Array<double>& dl, const Array<double>& bfl, Array<double>& lR, Array3<double>& lK, Array3<double>& lKd);
+    const Array<double>& dl, const Array<double>& bfl, const Array<double>& Kxi, Array<double>& lR, Array3<double>& lK, 
+    Array3<double>& lKd);
 
 void ustruct_3d_m(ComMod& com_mod, CepMod& cep_mod, const bool vmsFlag, const int eNoNw, const int eNoNq, 
     const int nFn, const double w, const double Je, const Vector<double>& Nw,  const Vector<double>& Nq, 
