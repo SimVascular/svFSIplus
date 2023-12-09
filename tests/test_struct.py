b01cf2b2efab784d178fe36aac695f6a0f26e8a6
Martin R. Pfaller <pfaller@stanford.edu>
2023-12-09 08:27:52 +0000

    Add Robin test

--- tests/test_struct.py
+++ tests/test_struct.py
@@ -8,6 +8,18 @@ from .conftest import run_with_reference
 # Common folder for all tests in this file
 base_folder = os.path.join("cases", "struct")
 
+# Fields to test
+fields = [
+    "Displacement",
+    "Velocity",
+    "Jacobian",
+    "Stress",
+    "Strain",
+    "Caucy_stress",
+    "Def_grad",
+    "VonMises_stress",
+]
+
 
 def test_LV_Guccione_passive(n_proc):
     folder = os.path.join(base_folder, "LV_Guccione_passive")
@@ -17,14 +29,9 @@ def test_LV_Guccione_passive(n_proc):
 
 def test_block_compression(n_proc):
     folder = os.path.join(base_folder, "block_compression")
-    fields = [
-        "Displacement",
-        "Velocity",
-        "Jacobian",
-        "Stress",
-        "Strain",
-        "Caucy_stress",
-        "Def_grad",
-        "VonMises_stress",
-    ]
+    run_with_reference(folder, fields, n_proc)
+
+
+def test_robin(n_proc):
+    folder = os.path.join(base_folder, "block_compression")
     run_with_reference(folder, fields, n_proc)
