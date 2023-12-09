b0cb42d87645258ffe03e522a6c4aba9dddd5198
Martin R. Pfaller <pfaller@stanford.edu>
2023-12-09 10:39:24 +0000

    Simplify paths, unify fields

--- tests/test_cmm.py
+++ tests/test_cmm.py
@@ -1,32 +1,28 @@
 import os
-import pytest
-
-import pandas as pd
 
 from .conftest import run_with_reference
 
 # Common folder for all tests in this file
-base_folder = os.path.join("cases", "cmm")
+base_folder = "cmm"
 
 
 def test_pipe_3d(n_proc):
-    folder = os.path.join(base_folder, "pipe_3d")
+    folder = "pipe_3d"
     inflate_folder = os.path.join(folder, "2a-inflate")
     fields = ["Displacement"]
     t_max = 3
-    run_with_reference(inflate_folder, fields, 1, t_max)
+    run_with_reference(base_folder, inflate_folder, fields, 1, t_max)
 
     inflate_cmm_folder = os.path.join(folder, "3a-inflate-cmm")
     fields = ["Displacement", "Pressure", "Velocity"]
     t_max = 5
-    run_with_reference(inflate_cmm_folder, fields, n_proc, t_max)
+    run_with_reference(base_folder, inflate_cmm_folder, fields, n_proc, t_max)
 
     prestress_folder = os.path.join(folder, "2b-prestress")
     fields = ["Stress"]
     t_max = 3
-    run_with_reference(prestress_folder, fields, 1, t_max)
+    run_with_reference(base_folder, prestress_folder, fields, 1, t_max)
 
     prestress_cmm_folder = os.path.join(folder, "3b-prestress-cmm")
-    fields = ["Displacement", "Stress", "Pressure", "Velocity"]
     t_max = 5
-    run_with_reference(prestress_cmm_folder, fields, n_proc, t_max)
+    run_with_reference(base_folder, prestress_cmm_folder, fields, n_proc, t_max)
--- tests/test_fluid.py
+++ tests/test_fluid.py
@@ -1,50 +1,44 @@
-import os
-import pytest
-
-import pandas as pd
-
 from .conftest import run_with_reference
 
 # Common folder for all tests in this file
-base_folder = os.path.join("cases", "fluid")
+base_folder = "fluid"
+
+# Fields to test
+fields = ["Velocity", "Pressure", "Traction", "WSS"]
 
 
 def test_pipe_RCR_3d(n_proc):
-    folder = os.path.join(base_folder, "pipe_RCR_3d")
-    fields = ["Pressure", "Velocity"]
-    run_with_reference(folder, fields, n_proc, 2)
+    test_folder = "pipe_RCR_3d"
+    t_max = 2
+    run_with_reference(base_folder, test_folder, fields, n_proc, t_max)
 
 
 def test_cavity_2d(n_proc):
-    folder = os.path.join(base_folder, "driven_cavity_2d")
-    fields = ["Pressure", "Velocity"]
-    run_with_reference(folder, fields, n_proc, 2)
+    test_folder = "driven_cavity_2d"
+    t_max = 2
+    run_with_reference(base_folder, test_folder, fields, n_proc, t_max)
 
 
 def test_dye_AD(n_proc):
-    folder = os.path.join(base_folder, "dye_AD")
-    fields = ["Velocity", "Pressure", "Traction", "WSS"]
-    run_with_reference(folder, fields, n_proc)
+    test_folder = "dye_AD"
+    run_with_reference(base_folder, test_folder, fields, n_proc)
 
 
 def test_newtonian(n_proc):
-    folder = os.path.join(base_folder, "newtonian")
-    fields = ["Velocity", "Pressure", "Traction", "WSS"]
-    run_with_reference(folder, fields, n_proc)
+    test_folder = "newtonian"
+    run_with_reference(base_folder, test_folder, fields, n_proc)
 
 
 def test_casson(n_proc):
-    folder = os.path.join(base_folder, "casson")
-    fields = ["Velocity", "Pressure", "Traction", "WSS"]
-    run_with_reference(folder, fields, n_proc)
+    test_folder = "casson"
+    run_with_reference(base_folder, test_folder, fields, n_proc)
 
 
 def test_carreau_yasuda(n_proc):
-    folder = os.path.join(base_folder, "carreau_yasuda")
-    fields = ["Velocity", "Pressure", "Traction", "WSS"]
-    run_with_reference(folder, fields, n_proc)
+    test_folder = "carreau_yasuda"
+    run_with_reference(base_folder, test_folder, fields, n_proc)
+
 
 def test_iliac_artery(n_proc):
-    folder = os.path.join(base_folder, "iliac_artery")
-    fields = ["Velocity", "Pressure", "Traction", "WSS"]
-    run_with_reference(folder, fields, n_proc)
+    test_folder = "iliac_artery"
+    run_with_reference(base_folder, test_folder, fields, n_proc)
