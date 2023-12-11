b7f215d481295329a85ab71b0e66208ee754d056
Martin R. Pfaller <pfaller@stanford.edu>
2023-12-11 21:33:11 +0000

    Simplify paths and unify fields in tests (#157)
    * Simplify paths, unify fields
    
    * Fix cmm

--- tests/test_heats.py
+++ tests/test_heats.py
@@ -6,13 +6,14 @@ import pandas as pd
 from .conftest import run_with_reference
 
 # Common folder for all tests in this file
-base_folder = os.path.join("cases", "heats")
+base_folder = "heats"
+
+# Fields to test
+fields = ["Temperature"]
 
 
-@pytest.mark.parametrize(
-    "name_inp", ["svFSI_CG.xml", "svFSI_BICG.xml", "svFSI_GMRES.xml"]
-)
-def test_diffusion_line_source(name_inp, n_proc):
-    folder = os.path.join(base_folder, "diffusion_line_source")
-    field = ["Temperature"]
-    run_with_reference(folder, field, n_proc, 2, name_inp=name_inp)
+@pytest.mark.parametrize("linear_solver", ["CG", "BICG", "GMRES"])
+def test_diffusion_line_source(linear_solver, n_proc):
+    test_folder = "diffusion_line_source"
+    name_inp = "svFSI_" + linear_solver + ".xml"
+    run_with_reference(base_folder, test_folder, fields, n_proc, 2, name_inp=name_inp)
