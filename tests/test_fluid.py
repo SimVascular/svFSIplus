85488795d4a2a21ede898132ef2d7043cd153399
kharold23 <52462115+kharold23@users.noreply.github.com>
2024-04-30 15:46:12 -0700

    Update test_fluid.py

--- tests/test_fluid.py
+++ tests/test_fluid.py
@@ -39,19 +39,18 @@ def test_pipe_RCR_genBC(n_proc):
             os.remove(name)
 
     # Compile genBC
-    os.chdir("genBC_svFSIplus")
+    os.chdir("genBC")
     subprocess.run(["make", "clean"], check=True)
     subprocess.run(["make"], check=True)
 
     # Change back to original directory
-    os.chdir("../../../../")
+    os.chdir("../../../../..")
 
     run_with_reference(base_folder, test_folder, fields, n_proc, t_max)
 
 def test_pipe_RCR_sv0D(n_proc):
     test_folder = "pipe_RCR_sv0D"
     t_max = 2
-    print(os.getcwd())
     run_with_reference(base_folder, test_folder, fields, n_proc, t_max)
 
 
