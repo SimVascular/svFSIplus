84a492e648b3d58754489be64917a3b37eaad288
aabrown100-git <aabrown100@hotmail.com>
2024-09-12 12:45:16 -0700

    Modifying viscosity test case and documentation

--- tests/cases/struct/tensile_adventitia_Newtonian_viscosity/README.md
+++ tests/cases/struct/tensile_adventitia_Newtonian_viscosity/README.md
@@ -1,8 +1,8 @@
 This test case simulates prescribed extension and compression of a slab of material
 described by the Neohookean material model and a Newtonian solid viscosity model.
 
-The load profile is sinusoidal over a period of 1 second with an amplitude
-of 100 dynes/cm^2. The load is applied on the Z1 face in the z-direction.
+The load profile is a 0.5s ramp to 1e5 dynes/cm^2, then held for another 0.5s. 
+The load is applied on the Z1 face in the z-direction.
 
 ![Load Profile](load.png)
 
@@ -11,4 +11,7 @@ The load data is defined in `load.dat`, which can be generated with
 
 The resulting deformation is shown in the video below:
 ![Deformation](movie.gif)
+
+The z-displacement of the Z1 face is plotted below:
+![Z1 Displacement](z1_face_displacement.png)
 
--- tests/cases/struct/tensile_adventitia_Newtonian_viscosity/generate_load.py
+++ tests/cases/struct/tensile_adventitia_Newtonian_viscosity/generate_load.py
@@ -11,9 +11,13 @@ n_modes = 256
 # Generate time values from 0 to 1
 time = np.linspace(0, 1, n_timesteps)
 
-# Generate sinusoidal load values with an amplitude of 100
-amplitude = 100
-load = amplitude * np.sin(2 * np.pi * time)
+# Generate 0.25s ramp to max load, then hold for 0.25s, then 0.25s ramp down to 0, then hold for 0.25s
+load = np.zeros(n_timesteps)
+max_load= -1e2
+load[time < 0.25] = max_load * time[time < 0.25] / 0.25
+load[(time >= 0.25) & (time < 0.5)] = max_load
+load[(time >= 0.5) & (time < 0.75)] = max_load * (1 - (time[(time >= 0.5) & (time < 0.75)] - 0.5) / 0.25)
+load[time >= 0.75] = 0
 
 # Write the time and load values to a text file
 with open("load.dat", "w") as file:
@@ -21,10 +25,10 @@ with open("load.dat", "w") as file:
     for t, s in zip(time, load):
         file.write(f"{t:.3f} {s:.3f}\n")
 
-# # Plot the load values
-# import matplotlib.pyplot as plt
-# plt.plot(time, load)
-# plt.xlabel("Time (s)")
-# plt.ylabel("load (dynes/cm^2)")
-# plt.title("Active load")
-# plt.savefig("load.png")
+# Plot the load values
+import matplotlib.pyplot as plt
+plt.plot(time, load)
+plt.xlabel("Time (s)")
+plt.ylabel("load (dynes/cm^2)")
+plt.title("Active load")
+plt.savefig("load.png")
