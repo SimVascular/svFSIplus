70419e4086a4f2b91b33ec0d7185be041a6171f8
divyaadil23 <divyaad@stanford.edu>
2024-09-17 14:34:26 -0700

    Changed input file names to svFSIplus.xml and responded to changes in comments.

--- Code/Source/svFSI/read_files.cpp
+++ Code/Source/svFSI/read_files.cpp
@@ -1801,7 +1801,7 @@ void read_files(Simulation* simulation, const std::string& file_name)
           if ((dmn.phys != EquationType::phys_ustruct) && (dmn.phys != EquationType::phys_struct)) { 
             continue; 
           }
-          if ((dmn.stM.isoType != ConstitutiveModelType::stIso_HO) || (dmn.stM.isoType != ConstitutiveModelType::stIso_HO_ma)) {
+          if ((dmn.stM.isoType != ConstitutiveModelType::stIso_HO)) {
             throw std::runtime_error("Active strain is allowed with Holzapfel-Ogden passive constitutive model only");
           }
         }
